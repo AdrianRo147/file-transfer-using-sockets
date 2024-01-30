2d381110af44b8e967b7d67e5c33cbf311ee4315
AdrianRo147 <adrian.rovnan@gmail.com>
2024-01-30 02:01:15 +0100

    client struct

--- client/client.h
+++ client/client.h
@@ -2,7 +2,22 @@
 // Created by Adrián on 30. 1. 2024.
 //
 
-#ifndef FILE_TRANSFER_SOCKETS_SERVER_CLIENT_H
-#define FILE_TRANSFER_SOCKETS_SERVER_CLIENT_H
+#include <sys/socket.h>
+#include <netinet/in.h>
 
-#endif //FILE_TRANSFER_SOCKETS_SERVER_CLIENT_H
+struct Client
+{
+    int domain;
+    int service;
+    int protocol;
+    u_long interface;
+    int port;
+
+    struct sockaddr_in address;
+
+    int socket;
+
+    void (*launch)(struct Client *client);
+};
+
+struct Client client_constructor(int domain, int service, int protocol, u_long interface, int port, void(*launch)(struct Client *client));
