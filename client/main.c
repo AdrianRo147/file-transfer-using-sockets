cf7e1e5b1d0de25d9c51e848f5bf2fa1c825db95
AdrianRo147 <adrian.rovnan@gmail.com>
2024-01-30 22:41:05 +0100

    bug fix

--- client/main.c
+++ client/main.c
@@ -11,9 +11,13 @@ void launch(struct Client *client)
     FILE *file = fopen("test.txt", "wb");
     char buffer[1024];
     ssize_t bytes;
+
     while((bytes = recv(client->socket, buffer, 1024, 0)) > 0)
     {
-        fwrite(buffer, 1, bytes, file);
+        if (fwrite(buffer, 1, bytes, file))
+        {
+            break;
+        }
     }
 
     if (bytes < 0)
@@ -29,7 +33,6 @@ void launch(struct Client *client)
 
 int main()
 {
-    printf("1\n");
     struct Client client = client_constructor(AF_INET, SOCK_STREAM, 0, INADDR_ANY, 8888, launch);
     client.launch(&client);
     return 0;
